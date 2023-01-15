1be9a07ce955586a684247236c64d7e45b6c9963
HongguLim <Honggu.Lim@outlook.com>
2023-01-16 01:39:12 +0900

    fix: styled-component bug fix

--- src/components/DetailBox.jsx
+++ src/components/DetailBox.jsx
@@ -38,33 +38,6 @@ function DetailBox() {
     navigate("/");
   };
 
-  const StyledDiv = styled.div`
-    background-color: #fff6e8;
-    padding: 20px;
-  `;
-
-  const StyledTable = styled.table`
-    border: 1px solid black;
-    margin-bottom: 20px;
-  `;
-
-  const StyledTh = styled.th`
-    border: 1px solid black;
-    padding: 5px;
-  `;
-
-  const StyledButton = styled.button`
-    width: 200px;
-    height: 50px;
-    background-color: darkslategray;
-    color: white;
-    font-weight: 700;
-    font-size: 17px;
-    border: 0;
-    border-radius: 13px;
-    cursor: pointer;
-  `;
-
   return (
     <StyledDiv>
       <h3>TODO 상세페이지</h3>
@@ -96,5 +69,30 @@ function DetailBox() {
     </StyledDiv>
   );
 }
+const StyledDiv = styled.div`
+  background-color: #fff6e8;
+  padding: 20px;
+`;
 
+const StyledTable = styled.table`
+  border: 1px solid black;
+  margin-bottom: 20px;
+`;
+
+const StyledTh = styled.th`
+  border: 1px solid black;
+  padding: 5px;
+`;
+
+const StyledButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: darkslategray;
+  color: white;
+  font-weight: 700;
+  font-size: 17px;
+  border: 0;
+  border-radius: 13px;
+  cursor: pointer;
+`;
 export default DetailBox;
